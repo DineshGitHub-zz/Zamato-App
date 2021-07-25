a4c15b21afddd733128c12115f9ff90bba81b076
Dinesh kumar <dineshkumar.athiappan@gmail.com>
2021-07-25 20:14:37 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ac2d0dbd-d96a-4e1a-9357-4d05035b8e27';
+export const MODAL_CLOSE_SEC = 2.5;
